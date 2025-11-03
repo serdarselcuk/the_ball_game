cf80f1d4f8bc64d053f2595da70eacdc289326b8
serdar selcuk <serdarselcu61@gmail.com>
2025-11-03 12:39:04 -0600

    start button performance upgraded

--- app/src/main/java/com/allfreeapps/theballgame/ui/composables/WelcomePage.kt
+++ app/src/main/java/com/allfreeapps/theballgame/ui/composables/WelcomePage.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,12 +14,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -30,6 +30,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
@@ -88,7 +89,11 @@ fun WelcomeScreen(
 
         // to draw a round button with text having same effect on the picture
         StartButton(
-            restartButtonOnClick = onStartButtonClicked,
+            modifier = Modifier
+                .clickable(
+                    true,
+                    onClick = onStartButtonClicked
+                ),
             initiateAnimatingForBoard = { buttonCreated = true }
         )
 
@@ -104,7 +109,6 @@ fun WelcomeScreen(
 @Composable
 fun StartButton(
     modifier: Modifier = Modifier,
-    restartButtonOnClick: () -> Unit = {},
     initiateAnimatingForBoard: () -> Unit = {}
 ) {
     val showText = remember { mutableStateOf(false) }
@@ -115,22 +119,8 @@ fun StartButton(
         targetSize = targetSize,
         color = MaterialTheme.colorScheme.surface,
         content = {
-            if (showText.value) {
-                ButtonWithText(
-                    Modifier
-                        .padding(2.dp)
-                        .width(targetSize)
-                        .height(targetSize),
-                    buttonText = stringResource(R.string.start_game),
-                    colors = ButtonColors(
-                        containerColor = MaterialTheme.colorScheme.surface,
-                        contentColor = Color.Blue,
-                        disabledContainerColor = MaterialTheme.colorScheme.surface,
-                        disabledContentColor = Color.Blue
-                    ),
-                    onclick = restartButtonOnClick
-                )
-            }
+            if (showText.value)
+                Text(stringResource(R.string.start_game))
         },
         animationCompleted = {
             showText.value = true
@@ -199,17 +189,33 @@ fun AnimatedWelcomingBall(
         }
     )
 
+
+    val radialGradient = Brush.radialGradient(
+        colors = listOf(Color.White.copy(alpha = 0.05f), color),
+        radius = animatedSize.value * 1.2f, // Use animatedSize for dynamic radius
+        center = Offset(
+            x = animatedSize.value * 0.9f, // Offset center for highlight effect
+            y = animatedSize.value * 0.9f
+        )
+    )
+
+
+
     Box(
         modifier
-            .background(color, shape = CircleShape)
+            .background(
+                brush = radialGradient,
+                shape = CircleShape
+            )
             .size(animatedSize),
         contentAlignment = Alignment.Center
     ) {
         if (animatedSize > 0.dp) {
+            content()
             Canvas(
                 modifier = Modifier.matchParentSize()
             ) {
-                val strokeWidth = 15.dp.toPx()
+                val strokeWidth = (animatedSize / 15).toPx()
                 drawArc(
                     color = Color.White,
                     startAngle = 210f,
@@ -225,22 +231,21 @@ fun AnimatedWelcomingBall(
                 )
             }
         }
-        content()
     }
 }
 
-@Preview(
-    showBackground = true,
-    showSystemUi = true,
-    name = "Welcome Page Portrait",
-    device = "spec:width=1200dp,height=2400dp,orientation=portrait"
-)
-@Composable
-fun PreviewWelcomePagePortrait() {
-    WelcomingBoard(
-        modifier = Modifier.offset(50.dp, 50.dp),
-    )
-}
+//@Preview(
+//    showBackground = true,
+//    showSystemUi = true,
+//    name = "Welcome Page Portrait",
+//    device = "spec:width=1200dp,height=2400dp,orientation=portrait"
+//)
+//@Composable
+//fun PreviewWelcomePagePortrait() {
+//    WelcomingBoard(
+//        modifier = Modifier.offset(50.dp, 50.dp),
+//    )
+//}
 
 @Preview(
     showBackground = true,
@@ -250,9 +255,5 @@ fun PreviewWelcomePagePortrait() {
 )
 @Composable
 fun PreviewWelcomePageStartButton() {
-    StartButton(
-        modifier = Modifier.offset(50.dp, 50.dp),
-        {},
-        {}
-    )
+
 }
